bb1d8dff4ef6e1fa6b7e295824a0498d42a2d4fb
Billy <billychorey@gmail.com>
2024-03-13 18:41:29 -0400

    cash register lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,36 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.last_transaction = []
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.last_transaction.append(
+            {'title': title, 'price': price, 'quantity': quantity}
+        ) 
+        self.items.extend([title] * quantity)
+
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = (self.discount / 100) * self.total
+            self.total = round(self.total - discount_amount)
+            print(f"After the discount, the total comes to ${self.total}.")
+        else:
+            print('There is no discount to apply.')
+
+    def void_last_transaction(self):
+        if not self.last_transaction:
+            print("No transactions to void.")
+        else:
+            self.total -= (
+                self.last_transaction[-1]['price'] * self.last_transaction[-1]['quantity']
+            )
+            # self.items.pop()  # Remove the last item from the list
+            for _ in range(self.last_transaction[-1]['quantity']):
+                self.items.pop()
+            self.last_transaction.pop()
+
+    def price_of_item(self, item):
+        return 0
--- lib/testing/cash_register_test.py
+++ lib/testing/cash_register_test.py
@@ -103,17 +103,17 @@ class TestCashRegister:
         assert(new_register.items == ["eggs", "eggs", "tomato", "tomato", "tomato"])
 
     def test_void_last_transaction(self):
-      '''subtracts the last item from the total'''
-      self.cash_register.add_item("apple", 0.99)
-      self.cash_register.add_item("tomato", 1.76)
-      self.cash_register.void_last_transaction()
-      assert(self.cash_register.total == 0.99)
-      self.reset_register_totals()
+        '''subtracts the last item from the total'''
+        self.cash_register.add_item("apple", 0.99)
+        self.cash_register.add_item("tomato", 1.76)
+        self.cash_register.void_last_transaction()
+        assert(self.cash_register.total == 0.99)
+        self.reset_register_totals()
 
     def test_void_last_transaction_with_multiples(self):
-      '''returns the total to 0.0 if all items have been removed'''
-      self.cash_register.add_item("tomato", 1.76, 2)
-      self.cash_register.void_last_transaction() 
-      assert(self.cash_register.total == 0.0)
-      self.reset_register_totals()
+        '''returns the total to 0.0 if all items have been removed'''
+        self.cash_register.add_item("tomato", 1.76, 2)
+        self.cash_register.void_last_transaction() 
+        assert(self.cash_register.total == 0.0)
+        self.reset_register_totals()
       
